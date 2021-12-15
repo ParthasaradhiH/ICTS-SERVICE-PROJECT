aa420175d039e41245a6202b23e9986bb8ef87bf
ParthasaradhiH <84763403+ParthasaradhiH@users.noreply.github.com>
2021-12-15 23:40:37 +0530

    Add files via upload

---
+++ Code/classdiagram/service.java
@@ -0,0 +1,12 @@
+package classdiagram;
+
+public class service {
+	private String service_name;
+	private int service_id;
+	
+	public String view_service() {
+		String s = "Select * from view_service";
+		return s;
+	}
+
+}
